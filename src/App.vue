ab41ada827faf25cc79c3d8ec85c11c86e74b42a
MASAHIRO-SANPEI\mfish <sanpei@egrowth-inc.com>
2024-09-28 22:43:36 +0900

    食事選択アプリの作成

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue'
+import FoodCard from './components/FoodCard.vue'
+const foods = ref([
+  { name: '寿司', emoji: '🍣' },
+  { name: 'ハンバーガー', emoji: '🍔' },
+  { name: '丼', emoji: '🍚' },
+  { name: 'オムライス', emoji: '🥚' },
+  { name: 'イタリアン', emoji: '🍝' },
+  { name: '中華料理', emoji: '🍜' },
+  { name: 'とんかつ', emoji: '🐖' },
+  { name: '韓国料理', emoji: '🫕' },
+  { name: '鍋系', emoji: '🍲' },
+  { name: '居酒屋', emoji: '🍶' },
+  { name: 'ファミレス', emoji: '🍴' }
+])
+
+const pickedFoodIndex = ref(0)
+const newFoodIndex = ref(1)
+
+function pickFood(index) {
+  pickedFoodIndex.value = index
+  newFoodIndex.value++
+}
+</script>
+
+<template>
+  <main>
+    <h1 class="title">今日何食べる？</h1>
+    <!-- V-bindで動的にnameとemojiを動的にする -->
+    <FoodCard
+      :name="foods[pickedFoodIndex].name"
+      :emoji="foods[pickedFoodIndex].emoji"
+      @click="pickFood(pickedFoodIndex)"
+    />
+    <template v-if="newFoodIndex < foods.length">
+      <p>VS</p>
+      <FoodCard
+        :name="foods[newFoodIndex].name"
+        :emoji="foods[newFoodIndex].emoji"
+        @click="pickFood(newFoodIndex)"
+      />
+    </template>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 350px;
+  margin: 100px auto;
+  text-align: center;
+}
+.title {
+  margin-bottom: 60px;
+}
+</style>
